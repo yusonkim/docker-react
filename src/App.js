c1caff49893a79a58e02ef2571de48995f403392
YUSONKIM <yuson.kim@glv.co.jp>
2019-04-08 21:47:35 +0900

    changed app text on feature branch

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I was changed on the Feature branch
           </a>
         </header>
       </div>
